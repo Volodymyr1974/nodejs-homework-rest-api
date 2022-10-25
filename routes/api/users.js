bd4d40ce00d8ac7831bba104e5d89f839e3d5683
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-10-25 22:14:56 +0300

    done

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,11 +3,16 @@ const User = require("../../models/user");
 const { RequestError } = require("../../helpers");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const { SECRET_KEY } = process.env;
 const authenticate = require("../../middlewares/authenticate");
-
+const upload = require("../../middlewares/upload");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 const Joi = require("joi");
+const path = require("path");
+
 const userSchema = Joi.object({
   email: Joi.string().required(),
   password: Joi.string().min(6).required(),
@@ -25,10 +30,12 @@ router.post("/register", async (req, res, next) => {
       throw RequestError(409, "Email in use");
     }
     const hashPassword = await bcrypt.hash(password, bcrypt.genSaltSync(10));
+    const avatarURL = gravatar.url(email);
     const result = await User.create({
       email,
       password: hashPassword,
       subscription,
+      avatarURL,
     });
     res.status(201).json({
       email: result.email,
@@ -73,7 +80,34 @@ router.get("/logout", authenticate, async (req, res, next) => {
   await User.findByIdAndUpdate(_id, { token: "" });
   res.status(204).json();
 });
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  async (req, res, next) => {
+    const { _id } = req.user;
+    const { path: tempUpload, originalname } = req.file;
 
+    const file = await Jimp.read(tempUpload);
+    await file.resize(250, Jimp.AUTO);
+    await file.writeAsync(tempUpload);
+    const filename = `${_id}${originalname}`;
+
+    const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+    try {
+      const resultUpload = path.join(avatarsDir, filename);
+      await fs.rename(tempUpload, resultUpload);
+      const avatarURL = path.join("avatars", filename);
+
+      await User.findByIdAndUpdate(_id, { avatarURL });
+      res.json({ avatarURL });
+    } catch (error) {
+      await fs.unlink(tempUpload);
+      next(error);
+    }
+  }
+);
 router.get("/current", authenticate, async (req, res, next) => {
   const { email, subscription } = req.user;
   res.json({
